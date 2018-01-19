0923443a52cdc49dab68a80aba55e9883924105f
Rishabh Verma <33465526+rishabh8cs@users.noreply.github.com>
2018-01-19 21:17:31 +0530

    Connects to database

--- Connection.php
+++ Connection.php
@@ -1,6 +1,6 @@
 <?php 
  $con=new mysqli("localhost","root","","cocktail");              // Create connection
-   if ($con->connect_error) {                                     // Check if  connection is eshtablished or 
+    if ($con->connect_error) {                                     // Check if  connection is eshtablished or 
            die("Connection failed: " . $con->connect_error); } 
 
   header("profile.php");
