78df366d20825d27cf5da5c830182dc3c28093b4
Rishabh Verma <33465526+rishabh8cs@users.noreply.github.com>
2018-01-19 21:39:01 +0530

    Update Connection.php

--- Connection.php
+++ Connection.php
@@ -1,5 +1,5 @@
 <?php 
- $con=new mysqli("localhost","root","","cocktail");              // Create connection
+ $con=new mysqli("localhost","root","","cocktail");              // Create  connection
     if ($con->connect_error) {                                     // Check if  connection is eshtablished or 
            die("Connection failed: " . $con->connect_error); } 
 
